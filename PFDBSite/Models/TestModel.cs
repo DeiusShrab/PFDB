b2dca57a1a6ede86df242f1f9935630f3e4bda00
NEOEXEUNTAR\zrats <zratsewk@yahoo.com>
2018-03-12 20:08:42 -0600

    PFDBSite rough outline

---
+++ PFDBSite/Models/TestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFDBSite.Models
+{
+    public class TestModel
+    {
+        public int numValue { get; set; }
+        public string strValue { get; set; }
+
+        public override string ToString()
+        {
+            return $"{strValue}, {numValue}";
+        }
+    }
+}
