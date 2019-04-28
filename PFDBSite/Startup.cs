92f249f08f0482500a5c2898f867de3f6057a9aa
NEOEXEUNTAR\zrats <zratsewk@yahoo.com>
2019-04-27 21:22:07 -0600

    more reference derp

--- PFDBSite/Startup.cs
+++ PFDBSite/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
